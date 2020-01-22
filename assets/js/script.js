f24460394d1f5b3f98e0bc397157c38242af5031
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-22 01:38:14 -0800

    FINALLY GOOD SHIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 let menu = document.querySelector(".mobile-menu");
 let btn = document.querySelector(".show-menu-btn");
 let navBtns = document.querySelectorAll(".mobile-btn");
-let container = document.getElementById("container-div");
-let closeBtn = document.getElementById("menu-close");
+let body = document.getElementById("global-styles");
 
 btn.addEventListener("click", function() {
     menu.setAttribute("style", "display: inline-block");
     let overlay = document.createElement("div");
-    overlay.setAttribute("style", "background-color: rgba(114, 107, 107, 0.5);")
-    overlay.setAttribute("style", "z-index: 40");
+    overlay.setAttribute("style", "background-color: rgba(114, 107, 107, 1.0);")
     overlay.setAttribute("style", "height: 100%");
     overlay.setAttribute("style", "width: 100%");
-    container.appendChild(overlay);
-});
-
-closeBtn.addEventListener("click", function() {
-    menu.setAttribute("style", "display: none");
+    body.appendChild(overlay);
 });
 
 navBtns.forEach(button => {
     button.addEventListener("click", function() {
         menu.setAttribute("style", "display: none");
-        container.setAttribute("style", "background-color: none");
+        body.setAttribute("style", "background-color: none");
     });
 });
