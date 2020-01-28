6fc7eccecea9755f926bede198cf251453c7b855
Mitchell-Johnson <mitchj9@uw.edu>
2020-01-27 16:21:32 -0800

    projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// mobile hamburger code
 let menu = document.querySelector(".mobile-menu");
 let btn = document.querySelector(".show-menu-btn");
 let navBtns = document.querySelectorAll(".mobile-btn");
@@ -16,5 +17,23 @@ navBtns.forEach(button => {
     button.addEventListener("click", function() {
         menu.setAttribute("style", "display: none");
         body.setAttribute("style", "background-color: none");
+    });
+});
+
+// more info on projects page code
+let projects = document.querySelectorAll("#project-card");
+
+
+projects.forEach(project => {
+    let elements = project.children;
+    elements[0].children[0].addEventListener("click", () => {
+        elements[0].setAttribute("style", "display: none");
+        elements[1].setAttribute("style", "display: block");
+        elements[2].setAttribute("style", "display: block");
+    });
+    elements[1].addEventListener("click", () => {
+        elements[0].setAttribute("style", "display: block");
+        elements[1].setAttribute("style", "display: none");
+        elements[2].setAttribute("style", "display: none");
     });
 });
