fa286d7e0e26553c72e1054209df02f848c271d5
Mitchell-Johnson <mitchelljoho@gmail.com>
2023-01-01 13:35:02 -0800

    fixed

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ const port = process.env.PORT || 8080
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '/index.html'))
-})
-
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
